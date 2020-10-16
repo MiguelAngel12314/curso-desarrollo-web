8727381e71d079ff8f7d9afdbf39001f77618245
Ricardo <rich.gzz11@gmail.com>
2020-10-16 17:08:52 -0500

    NUEVO

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-document.write("Hola Mundo");
+var nombre = document.getElementById('nombre');
+var Showname = document.getElementById('minombre');
+var apellido = document.getElementById('apellido');
+var Showapellido = document.getElementById('miapellido');
+
+function enviar(){
+    Showname.innerHTML=nombre.value;
+    console.log(nombre.value);
+    Showapellido.innerHTML=apellido.value;
+    console.log(apellido.value);
+}
+
+function limpiar(){
+    nombre.value="";
+    Showname.innerHTML="";
+
+    apellido.value="";
+    Showapellido.innerHTML="";
+}
