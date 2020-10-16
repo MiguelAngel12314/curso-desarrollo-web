7b1b01cc953733762d002eccb9243b02f834a011
David <chinos_26mimo@hotmail.com>
2020-10-16 16:50:20 -0500

    estructura js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-document.write("Hola mundo");
+var nombre = document.getElementById('nombres');
+var ShowName = document.getElementById('minombre');
+var apellido = document.getElementById('apellidos');
+var ShowApellido = document.getElementById('miapellido');
+
+function enviar(){
+    ShowName.innerHTML=nombre.value;
+    console.log(nombre.value);
+    
+
+    console.log(apellido.value);
+    ShowApellido.innerHTML = apellido.value;
+}
+
+function limpiar(){
+nombre.value = "";
+ShowName.innerHTML="";
+apellido.value ="";
+ShowApellido.innerHTML ="";
+}
