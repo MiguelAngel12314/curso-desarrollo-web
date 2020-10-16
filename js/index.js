d105c9bbc3fe7c81508dd97e229137e605cf4cfd
Ricardo <rich.gzz11@gmail.com>
2020-10-16 17:23:48 -0500

    modificación

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
 var nombre = document.getElementById('nombre');
-var Showname = document.getElementById('minombre');
+var showName = document.getElementById('minombre');
 var apellido = document.getElementById('apellido');
-var Showapellido = document.getElementById('miapellido');
+var showaAellido = document.getElementById('miapellido');
 
 function enviar(){
-    Showname.innerHTML=nombre.value;
+    showName.innerHTML=nombre.value;
     console.log(nombre.value);
-    Showapellido.innerHTML=apellido.value;
+    showApellido.innerHTML=apellido.value;
     console.log(apellido.value);
 }
 
 function limpiar(){
     nombre.value="";
-    Showname.innerHTML="";
+    showName.innerHTML="";
 
     apellido.value="";
-    Showapellido.innerHTML="";
+    showApellido.innerHTML="";
 }
