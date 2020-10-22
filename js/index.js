2e8712139469306674acc6447e34dbcede1147d9
Ricardo <rich.gzz11@gmail.com>
2020-10-21 19:20:53 -0500

    Correo_y_Contraseña

--- js/index.js
+++ js/index.js
@@ -1,13 +1,24 @@
 var nombre = document.getElementById('nombre');
 var showName = document.getElementById('minombre');
 var apellido = document.getElementById('apellido');
-var showaAellido = document.getElementById('miapellido');
+var showApellido = document.getElementById('miapellido');
+var correo = document.getElementById('correo');
+var showCorreo = document.getElementById('micorreo');
+var contraseña = document.getElementById('contraseña');
+var showContraseña = document.getElementById('micontraseña');
 
 function enviar(){
     showName.innerHTML=nombre.value;
     console.log(nombre.value);
+
     showApellido.innerHTML=apellido.value;
     console.log(apellido.value);
+
+    showCorreo.innerHTML=correo.value;
+    console.log(correo.value);
+
+    showContraseña.innerHTML=contraseña.value;
+    console.log(contraseña.value);
 }
 
 function limpiar(){
@@ -16,4 +27,10 @@ function limpiar(){
 
     apellido.value="";
     showApellido.innerHTML="";
+
+    correo.value="";
+    showCorreo.innerHTML="";
+
+    contraseña.value="";
+    showContraseña.innerHTML="";
 }
