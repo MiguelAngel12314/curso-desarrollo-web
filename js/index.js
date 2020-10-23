30312dcf20d9d107cab242f039d2c2c969263c1a
David <chinos_26mimo@hotmail.com>
2020-10-23 16:55:32 -0500

    Selecct funcinando

--- js/index.js
+++ js/index.js
@@ -2,14 +2,20 @@ var nombre = document.getElementById('nombres');
 var ShowName = document.getElementById('minombre');
 var apellido = document.getElementById('apellidos');
 var ShowApellido = document.getElementById('miapellido');
-
+var radiooptionsexo = document.getElementsByName('sexo');
+var radiosex = document.getElementsByName('radiotiposexo');
+var checkboxsex = document.getElementById('tiposexo');
+var checkboxsextipe = document.getElementById('checkboxtiposexo');
+var estadoselec = document.getElementById('seleccion');
+var selecedo= document.getElementById('estadovalor') ;
 function enviar(){
     ShowName.innerHTML=nombre.value;
     console.log(nombre.value);
-    
-
+    condicional();
+    radiosex.innerHTML = radiooptionsexo.value;
     console.log(apellido.value);
     ShowApellido.innerHTML = apellido.value;
+    selecedo.innerHTML= estadoselec.value;
 }
 
 function limpiar(){
@@ -18,3 +24,10 @@ ShowName.innerHTML="";
 apellido.value ="";
 ShowApellido.innerHTML ="";
 }
+ function condicional(){
+     if(checkboxsex.checked){
+         checkboxsextipe.innerHTML='Hombre';
+     }else{
+         checkboxsextipe.innerHTML='Mujer';
+     }
+ }
